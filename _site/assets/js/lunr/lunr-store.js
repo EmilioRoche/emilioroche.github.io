951578114b3d91056333d5ae5d35199b8d5b9295
EmilioRoche <32501779+EmilioRoche@users.noreply.github.com>
2021-10-28 14:30:00 -0300

    Delete _site directory

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "first post",
-        "excerpt":" ","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "/jekyll/update/2021/10/11/welcome-to-jekyll.html",
-        "teaser": null
-      }]
